7d4be925b10cd3d124f39413464c5e144f1fdfa1
albeiro9712 <104403557+albeiro9712@users.noreply.github.com>
2022-08-22 20:38:07 -0500

    estructura del proyecto

---
+++ src/test/java/com/test/bancolombia/BancolombiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.bancolombia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancolombiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
